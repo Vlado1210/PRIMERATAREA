3afde635ebbbee6d7acb016b396d07933d810b22
vlady <vlady@LAPTOP-1G376GC0>
2023-01-09 21:30:34 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vladimir Chavez 166045");
+            Console.WriteLine("Graficacion y videojuegos");
+        }
+    }
+}
